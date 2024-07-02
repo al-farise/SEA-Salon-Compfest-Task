f166b3ae1d3f75c5e147964f4e1a39a7b9b12461
Muhammad Salman Al Farisi <al_farisi@webmail.umm.ac.id>
2024-07-02 19:35:24 +0700

    configure alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import path from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
 })
